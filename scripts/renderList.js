f7f18c7354299bfae0fd44877b80fefabff9050f
Mariano-godoy <godoyjosemariano@hotmail.com>
2024-06-22 08:07:39 -0300

    array products

---
+++ scripts/renderList.js
@@ -0,0 +1,18 @@
+const productos = [
+    {
+        "name": "Kit de microscopio digital Celestron",
+        "imageURL": "https://www.ozscopes.com.au/pub/media/catalog/product/cache/45eb0a64749ceaa7c5482dc7c014d2da/c/e/celestron-digital-microscope-kit-disc_1.jpg",
+        "description": [
+            "Uso como microscopio biológico, tradicional o digital", 
+            "Ampliación de amplio rango",
+             "Fácil de configurar",
+              "Incluye el accesorio de la cámara digital y el software de la computadora",
+               "3 diapositivas preparadas",
+                "Ligero y portátil",
+                 "Garantía limitada de 2 años",
+                ],
+        "price": "U$D 149.95",
+      },
+      
+                    
+]
