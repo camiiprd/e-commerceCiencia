b844fa5deb59b1cd5b425071f5af3ef8dfa41ad5
camiiprd <camila.prado.031@gmail.com>
2024-06-24 02:30:46 -0300

    We added the user icon and a logout to the  navbar.

--- scripts/login.js
+++ scripts/login.js
@@ -4,40 +4,63 @@ const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 
 document.addEventListener('DOMContentLoaded', async () => {
-    try {
-        const response = await fetch(urlUsers);
-        const users = await response.json();
-        localStorage.setItem('users', JSON.stringify(users));
-    } catch (error) {
-        console.error('Error al cargar usuarios', error);
+    await cargarUsers()
+    mostrarUsers()
+
+    if (loginForm) {
+        loginForm.addEventListener('submit', async (event) => {
+            event.preventDefault()
+            await login()
+        })
     }
+})
 
-    loginForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        await login(); // Esperar a que la función login termine
-    });
-});
-
-async function login() {
-    const email = emailInput.value;
-    const password = passwordInput.value;
-
-    const users = JSON.parse(localStorage.getItem('users'));
-    const user = users.find(user => user.email === email);
-
-    if (user) {
-        if (user.password === password) {
-            // Login exitoso, redirigir al index
-            window.location.href = '../index.html';
-        } else {
-            showError('Correo o Contraseña incorrecto');
-        }
-    } else {
-        showError('Correo o contraseña incorrecto');
+async function cargarUsers(){
+    try {
+        const response = await fetch(urlUsers)
+        const users = await response.json()
+        localStorage.setItem('users', JSON.stringify(users))
+    } catch (error) {
+        console.error('error al cargar usuario', error)
     }
 }
 
-function showError(message) {
-    const messageElement = document.getElementById('message');
-    messageElement.textContent = message;
+async function login(){
+    const email = emailInput.value
+    const password = passwordInput.value
+
+    const users = JSON.parse(localStorage.getItem('users'))
+    const user = users.find(user => user.email === email)
+
+    if (user.password === password) {
+
+        localStorage.setItem('logUser', JSON.stringify({
+            name: user.name,
+            lastname: user.lastname,
+            email: user.email
+        }))
+
+        window.location.href = '../index.html'
+        
+    } else {
+        showError('correo o contraseña incorrectos')
+    }
+}
+
+function showError(message){
+    const messageError = document.getElementById('message')
+    messageError.textContent = message
+}
+
+async function mostrarUsers(){
+    const logUser = JSON.parse(localStorage.getItem('logUser'))
+    if (logUser) {
+        const nameUsers = `${logUser.name} ${logUser.lastname} ${logUser.email}`
+        document.getElementById('username').textContent = nameUsers
+    }
+}
+
+async function logout(){
+    localStorage.removeItem('logUser')
+    window.location.href = './pages/login.html'
 }
